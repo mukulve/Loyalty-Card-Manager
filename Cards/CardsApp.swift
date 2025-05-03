f0e0ffd82a8f0ba9cbd884641cc71050740f038b
mukul <mukul@uoguelph.ca>
2025-05-03 09:22:57 -0400

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Mukul Verma on 2025-05-03.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardsTests/CardsTests.swift
@@ -0,0 +1,17 @@
+//
+//  CardsTests.swift
+//  CardsTests
+//
+//  Created by Mukul Verma on 2025-05-03.
+//
+
+import Testing
+@testable import Cards
+
+struct CardsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
